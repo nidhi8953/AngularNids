10b79c3fbcc80f3308a956c0190698ca00f37cb0
nidhi8953 <50191087+nidhi8953@users.noreply.github.com>
2025-05-03 12:42:26 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-allure-reporter')
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
@@ -44,7 +45,7 @@ module.exports = function (config) {
         ]
       }
     },
-      allureReport: {
+    allureReport: {
       reportDir: 'allure-results',
       useBrowserName: false
     }
